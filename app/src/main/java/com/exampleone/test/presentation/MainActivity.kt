bf02ca164c91e9e53466904840f92d1c06334a12
Николай <kolyas56atm@gmail.com>
2022-09-10 23:59:30 +0300

    added night mode

--- app/src/main/java/com/exampleone/test/presentation/MainActivity.kt
+++ app/src/main/java/com/exampleone/test/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.exampleone.test.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import com.exampleone.test.R
 import com.exampleone.test.databinding.ActivityMainBinding
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         // binding?.bottomMainMenu?.selectedItemId = R.id.homeBottomMainMenu
